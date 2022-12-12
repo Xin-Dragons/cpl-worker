9bf1263349e7b90af873d8915b91dabbd885abd1
Joe Fitter <hello@joefitter.com>
2022-12-12 13:07:46 +0100

    logging

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,12 @@ import bodyParser from 'body-parser';
 export const app = express();
 
 app.post('/', bodyParser.json(), (req, res, next) => {
-  console.log(req.body);
+  const [event] = req.body;
+  const nft = event.events.nft;
+
+  console.log(nft);
+  console.log(event.signature)
+  console.log(event.tokenTransfers[0])
+
   next();
 })
