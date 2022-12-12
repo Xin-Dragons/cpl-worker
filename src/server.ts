860a26b178df62cbb36fba34c943cb7492dc0dad
Joe Fitter <hello@joefitter.com>
2022-12-12 22:24:02 +0100

    added logging

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,9 @@ export const app = express();
 
 app.post('/', bodyParser.json(), async (req, res, next) => {
   const [event] = req.body;
-
+  
   const { amount, buyer, seller, nfts, signature } = event.events.nft;
+  console.log('received event', signature)
   const price = amount / LAMPORTS_PER_SOL;
 
   await nfts.map((item: any) => recordSale({ mint: item.mint, signature, price, buyer, seller }));
